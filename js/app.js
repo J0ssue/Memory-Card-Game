0ed6d8b77e8128cb049340d95a6e943571650597
J0ssue <jdiaz66@icloud.com>
2018-03-30 14:33:25 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const heart = document.querySelectorAll(".hearts li");
+const restart = document.querySelector(".fa-redo-alt"); 
+const deck = document.querySelector(".deck");
+const cards = document.querySelectorAll("li.card")
+
+deck.addEventListener("click", function(e) {
+	if(e.target.className === "card") {
+		e.target.style.fontSize = "30px";		
+		e.target.style.color = "#000";		
+		e.target.style.backgroundColor = "aqua";
+		}
+});
